6763210102aee0614ee241591bcbe1b3c5188af5
Sechere <jermains@computercastlesltd.co.ke>
2019-09-03 10:30:35 +0300

    [03/09/2019] first commit

---
+++ Employee.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrialEmployeePayroll
+{
+    class Employee
+    {
+        public string name { set; get; }
+        public double loan_amount { set; get; }
+        public double gross_pay { set; get; }
+        // public double loan_Deductable { set; get; }
+
+        public Employee(string name, double gross_pay, double loan_amount)
+        {
+            this.name = name;
+            this.loan_amount = loan_amount;
+            this.gross_pay = gross_pay;
+
+        }
+
+        public double CalculateLoanDeductable(double gross_pay, double loan_amount)
+        {
+
+
+
+            if (loan_amount <= 30000)
+            {
+                return gross_pay * 0.05;
+            }
+            else if (loan_amount > 30000 && loan_amount < 100000)
+            {
+                return gross_pay * 0.1;
+            }
+            else if (loan_amount >= 100000)
+            {
+                return gross_pay * 0.2;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        public double CalculateNetSalary(double gross_pay, double loan_deductable)
+        {
+
+            return gross_pay - loan_deductable;
+
+        }
+        public double CalculateRemainingLoanAmount(double loan_amount, double loan_deductable)
+        {
+            return loan_amount - loan_deductable;
+        }
+    }
+}
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrialEmployeePayroll
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> employees = new List<Employee>();
+            
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter the name of the customer");
+
+                string name = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Enter the salary of the customer");
+                double gross_pay = 0;
+                while (!double.TryParse(Console.ReadLine(), out gross_pay))
+                {
+                    Console.WriteLine("You have entered an invalid input");
+                    Console.WriteLine("Please enter a valid salary");
+                }
+                if (gross_pay < 0)
+                {
+                    Console.WriteLine("You can not enter a NEGATIVE value as salary");
+                    Console.WriteLine("Please enter a valid salary");
+                    while (!double.TryParse(Console.ReadLine(), out gross_pay))
+                    {
+                        Console.WriteLine("You have entered an invalid input");
+                        Console.WriteLine("Please enter a valid salary");
+                    }
+
+
+                }
+
+                Console.WriteLine("Enter the loan  of the customer");
+                double loan_amount = 0;
+                while (!double.TryParse(Console.ReadLine(), out loan_amount))+
+                {
+                    Console.WriteLine("You have entered an invalid input");
+                    Console.WriteLine("Please enter a loan amount");
+                }
+                if (loan_amount < 0)
+                {
+                    Console.WriteLine("You can not enter a NEGATIVE value as salary");
+                    Console.WriteLine("Please enter a valid salary");
+                    while (!double.TryParse(Console.ReadLine(), out loan_amount))
+                    {
+                        Console.WriteLine("You have entered an invalid input");
+                        Console.WriteLine("Please enter a valid salary");
+                    }
+
+
+                }
+                Employee employee = new Employee(name, gross_pay, loan_amount);
+                employees.Add(employee);
+            }
+
+            foreach (var item in employees)
+            {
+                Console.WriteLine("Employee name : {0}",item.name);
+                Console.WriteLine("Employee Gross Salary : {0}", item.gross_pay);
+                Console.WriteLine("Employee Loan Amount : {0}", item.loan_amount);
+
+                Console.WriteLine("Employee Loan Deductable : {0}", item.CalculateLoanDeductable(item.gross_pay, item.loan_amount));
+
+                
+                double net_salary = item.CalculateNetSalary(item.gross_pay, item.CalculateLoanDeductable(item.gross_pay, item.loan_amount));
+                double balance = item.CalculateRemainingLoanAmount(item.loan_amount, item.CalculateLoanDeductable(item.gross_pay, item.loan_amount));
+
+                if (balance<0)
+                {
+                    double bal;
+                    bal = balance * -1;
+                    net_salary = net_salary + bal;
+                    balance = 0;
+                }
+
+                Console.WriteLine("Employee Net Salary : {0}", net_salary);
+                Console.WriteLine("Employee's Remaining Loan Balance : {0}",balance);
+            }
+
+
+
+
+
+
+        }
+    }
+}
